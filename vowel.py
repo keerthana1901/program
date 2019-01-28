398173a1b1f1cb12497b7b0f6c55e055d9790dbf
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-01-28 16:34:03 +0530

    Create vowel.py

---
+++ vowel.py
@@ -0,0 +1,5 @@
+given=raw_input()
+if (given=='A' or given=='a' or given=='e' or given=='E' or given=='I' or given=='i' or given=='O' or given=='o' or given=='U' or given=='u' ):
+	print "Vowel"
+else:
+	print "Consonant"
