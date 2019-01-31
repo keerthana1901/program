1c61aeab7fa0e5b76a6575d99d4b66126c562e90
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-02-01 00:06:06 +0530

    Update prime.py

--- prime.py
+++ prime.py
@@ -1,14 +1,10 @@
-a=raw_input()
-a=a.split()
-lower=int(a[0])
-upper=int(a[1])
-prime=[]
-for num in range(lower+1, upper):
-    if num > 1:
-        for i in range(2, num):
-            if (num % i) == 0:
-                break
-        else:
-            prime.extend(str(num))
-prime=' '.join(prime)
-print prime
+usr=int(raw_input())
+if(usr>1):
+	for i in range(2,usr):
+		if(usr%i)==0:
+			print 'no'
+			break
+	else:
+		print 'yes'
+else:
+	print 'no'
