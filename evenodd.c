c6f2289bac7b0bab79b62ecb122a005ba47e45a3
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-02-13 20:22:42 +0530

    Update evenodd.c

--- evenodd.c
+++ evenodd.c
@@ -1,34 +1,19 @@
-/* package whatever; // don't place package name! */
+#include <stdio.h>
 
-import java.util.*;
-import java.lang.*;
-import java.io.*;
-
-
-class evenodd
-{
-	public static void main (String[] args) throws java.lang.Exception
+int main(void) {
+	int a[100],n,i,j,k,t;
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
 	{
-		int n;
-     Scanner s=new Scanner(System.in);
-     n=s.nextInt();
-     int a[]=new int[n];
-     for(int i=0;i<n;i++){
-         a[i]=s.nextInt();
-         
-     }
-     for(int i=0;i<n;i++){
-         if(i%2==0){
-             if(a[i]%2!=0){
-                 System.out.print(a[i]);
-             }
-         }
-         else if(i % 2 != 0)
-         {
-             if(a[i]%2==0){
-                 System.out.println(a[i]);
-             }
-         }
-     }
+		scanf("%d",&a[i]);
 	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]%2!=0 && (i+1)%2!=0||(a[i]%2==0 && (i+1)%2==0))
+		{
+			printf("%d ",a[i]);
+		}
+	}
+	return 0;
 }
+
