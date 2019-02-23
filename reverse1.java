c82211027dc75627aa788d0e10ac8532e72cb2a6
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-02-23 20:12:35 +0530

    Create reverse1.java

---
+++ reverse1.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+			   String s1="hello world";
+	   String s2=new StringBuffer(s1).reverse().toString();
+	   System.out.println(s2);
+
+	}
+}
