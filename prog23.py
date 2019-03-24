c2dc7a4e39920541410791a7e0eb1fb3cef67974
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-03-24 23:34:56 +0530

    Update prog23.py

--- prog23.py
+++ prog23.py
@@ -1,13 +1,18 @@
 # your code goes here
-import sys, string, math
-M1= int(input())
-O1 = 2**M1
-S1 = []
-for i in range(0,O1) :
-    T1 = bin(i)[2:]
-    j = len(T1)
-    if j < M1 :
-        T1 = '0' * (M1-j) + T1
-    S1.append(T1)
-for i in range(0,len(S1)) :
-    print(S1[i])
+a=int(input())
+d1,m1=[],[]
+for i in range(0,2**a):
+  b='{:2b}'.format(i)
+  if len(b)<a:
+    c='0'*(a-len(b))+b
+    d1.append(c)
+  else:
+    d1.append(b[-a:])
+for i in range(0,len(d1)):
+  p=list(d1[i])
+  if ' ' in p:
+    k=p.index(' ')
+    p[k]='0'
+  m1.append(''.join(p))
+for i in m1:
+  print(i)
