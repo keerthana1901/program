ecca144375aaef3448e727ea2edcc79d01ba4902
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-03-24 20:23:29 +0530

    Create prog11.java

---
+++ prog11.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class sub 
+{
+public static void main(String[] args)
+  {
+		int d,b,t;
+		System.out.println("first number");
+		Scanner s=new Scanner(System.in);
+		d=s.nextInt();
+		System.out.println("second number");
+		b=s.nextInt();
+		System.out.println("subtraction is");
+		t=d-b;
+		System.out.println(t);
+	}
+}
