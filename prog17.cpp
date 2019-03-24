2e8177cd9f06b8ba109064b09e5e29d37ee15342
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-03-24 21:42:08 +0530

    Update prog17.cpp

--- prog17.cpp
+++ prog17.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 
 int main(){
-    int n, m, temp, max_count=0;
-    cin>>n>>m;
+    int p, q, temp, max_count=0;
+    cin>>p>>q;
 
-    vector<vector<int>> count(n+1, vector<int>(m+1));
-    for(int i=0; i<n; i++){
-        for(int j=0; j<m; j++){
+    vector<vector<int>> count(p+1, vector<int>(q+1));
+    for(int i=0; i<p; i++){
+        for(int j=0; j<q; j++){
             cin>>temp;
             if(i==0 || j==0 || temp == 0){
                 count[i+1][j+1] = temp;
