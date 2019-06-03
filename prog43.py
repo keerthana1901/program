71076e9e7c1e0127d4c582e5808dddbf6918096d
keerthana1901 <46592145+keerthana1901@users.noreply.github.com>
2019-06-03 17:22:46 +0530

    Create prog43.py

---
+++ prog43.py
@@ -0,0 +1,44 @@
+def find(temp,d):
+  avail=sorted(d.keys())
+  for i in range(len(avail)):
+    if temp in avail:
+      temp+=1
+    else:
+      break
+  return temp
+n1=int(input())
+a=list(map(int,input().split()))
+d={}
+c=0
+for i in range(n1):
+  if a[i] not in d.keys():
+    d[a[i]]=1
+  else:
+    d[a[i]]+=1
+temp=1
+temp=find(temp,d)
+ins=[]
+for i in range(n1):
+  if d[a[i]]==1:
+    ins.append(a[i])
+for i in range(n1):
+  if d[a[i]]==1:
+    ins.append(a[i])
+  elif a[i] in ins:
+    d[a[i]]-=1
+    a[i]=temp
+    ins.append(temp)
+    d[temp]=1
+    temp=find(temp+1,d)
+    c+=1
+  elif a[i]>temp:
+    d[a[i]]-=1
+    a[i]=temp
+    ins.append(temp)
+    d[temp]=1
+    temp=find(temp+1,d)
+    c+=1
+  else:
+    ins.append(a[i])
+print(c)
+print(*a)
